67330d41d2c5c4e7778628f0efa27691c03bc8bf
Jeff Kempe <jkempe@protonmail.com>
2023-07-24 09:49:30 -0400

    Demo ParameterizedThreadStart class

---
+++ AddWithThreads/AddParams.cs
@@ -0,0 +1,12 @@
+﻿namespace AddWithThreads;
+
+public class AddParams
+{
+    public int a, b;
+
+    public AddParams(int a, int b)
+    {
+        this.a = a;
+        this.b = b;
+    }
+}
---
+++ AddWithThreads/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AddWithThreads;
+
+Console.WriteLine("***** Add With Threads *****\n");
+
+Console.WriteLine($"ID of thread in Main: {Environment.CurrentManagedThreadId}");
+AddParams addParams = new(20, 10);
+Thread t = new(new ParameterizedThreadStart(Add));
+t.Start(addParams);
+
+Thread.Sleep(1000);
+Console.WriteLine("Press <Enter> to continue...");
+Console.ReadLine();
+
+void Add(object data)
+{
+    if (data is AddParams ad)
+    {
+        Console.WriteLine($"-> on thread (ID): {Environment.CurrentManagedThreadId}");
+        Console.WriteLine($"{ad.a} + {ad.b} = {ad.a + ad.b}");
+    }
+}
