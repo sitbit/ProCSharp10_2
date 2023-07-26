50784b52a4531b5f21f5d416dee393618e9bd78c
Jeff Kempe <jkempe@protonmail.com>
2023-07-26 08:11:27 -0400

    Demo .Net thread pool

---
+++ ThreadPoolApp/Printer.cs
@@ -0,0 +1,21 @@
+﻿namespace ThreadPoolApp;
+
+public class Printer
+{
+    private readonly object threadLock = new();
+    public void PrintNumbers()
+    {
+        lock (threadLock)
+        {
+            Console.WriteLine($"-> {Thread.CurrentThread.ManagedThreadId} is executing PrintNumbers()");
+            Console.WriteLine("Your numbers:");
+            for (int i = 0; i < 10; ++i)
+            {
+                Random r = new();
+                Thread.Sleep(100 * r.Next(5));
+                Console.Write($"{i}, ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ ThreadPoolApp/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ThreadPoolApp;
+
+Console.WriteLine("***** Thread Pool App *****\n");
+
+Console.WriteLine($"Main thread started; ID: {Environment.CurrentManagedThreadId}");
+Printer p = new();
+
+WaitCallback workItem = new(PrintTheNumbers);
+
+for (int i = 0; i < 10; ++i)
+{
+    _ = ThreadPool.QueueUserWorkItem(workItem, p);
+}
+
+Console.WriteLine("Press <Enter> to continue...");
+Console.ReadLine();
+
+static void PrintTheNumbers(object state)
+{
+    Printer task = (Printer)state;
+    task.PrintNumbers();
+}
