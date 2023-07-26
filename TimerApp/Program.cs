d6700820d9e19f061ccb0ee1bf7e07d2454e0ac7
Jeff Kempe <jkempe@protonmail.com>
2023-07-26 08:10:56 -0400

    Demo Timer class

---
+++ TimerApp/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("***** Timer App *****");
+
+TimerCallback timerCallback = new(PrintTime);
+
+_ = new Timer(
+    timerCallback,
+    "Hello, world!",
+    0,
+    1000
+    );
+
+Console.WriteLine("Press <Enter> to stop...");
+Console.ReadLine();
+
+static void PrintTime(object state)
+{
+    Console.WriteLine($"{state}, the time is: {TimeOnly.FromDateTime(DateTime.Now).ToLongTimeString()}");
+}
