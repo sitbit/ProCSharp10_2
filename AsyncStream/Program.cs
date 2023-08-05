9b551a6e9f542c7da54173cd1557bfb69bb57a77
Jeff Kempe <jkempe@protonmail.com>
2023-08-05 11:43:38 -0400

    Demo async streams

---
+++ AsyncStream/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("***** Async Stream *****");
+
+await foreach (int number in GenerateSequence())
+{
+    Console.WriteLine(number);
+}
+
+static async IAsyncEnumerable<int> GenerateSequence()
+{
+    for (int i = 0; i < 20; ++i)
+    {
+        await Task.Delay(300);
+        yield return i;
+    }
+}
