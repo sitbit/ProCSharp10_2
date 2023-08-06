b27c0c86759d91290b77b2fc5e0ec717f5ed0462
Jeff Kempe <jkempe@protonmail.com>
2023-08-06 10:44:36 -0400

    Complete CustomNamespaces code

---
+++ CustomNamespaces/My3DShapes/Circle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomNamespaces.My3DShapes
+{
+    internal class Circle
+    {
+        public Circle() { }
+    }
+}
