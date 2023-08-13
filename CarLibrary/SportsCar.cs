ec6ca23a1c7e016a10413401a81e85a883f83770
Jeff Kempe <jkempe@protonmail.com>
2023-08-13 10:36:12 -0400

    Update to CarLibrary.SportsCar (radio)

--- CarLibrary/SportsCar.cs
+++ CarLibrary/SportsCar.cs
@@ -2,11 +2,22 @@
 
 public class SportsCar : Car
 {
+    MusicMediaEnum music = MusicMediaEnum.MusicCD;
+    bool listeningToMusic = false;
     public SportsCar() { }
     public SportsCar(string name, int currSpeed, int maxSpeed) : base(name, currSpeed, maxSpeed) { }
 
     public override void TurboBoost()
     {
         Console.WriteLine("Ramming speed! Faster is better...");
+    }
+    public void TurnOnRadio(bool? musicOn, MusicMediaEnum media)
+    {
+        if (musicOn != null)
+        {
+            music = media;
+            listeningToMusic = musicOn == true;
+        }
+        Console.WriteLine(listeningToMusic ? $"Jamming {music}" : "Quiet time...");
     }
 }
