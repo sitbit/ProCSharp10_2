ed8d4ec7baf75f3ac1ee65656662bf6b774789bf
Jeff Kempe <jkempe@protonmail.com>
2023-08-24 10:39:02 -0400

    Finish Dynamic Keyword demo

--- DynamicKeyword/Program.cs
+++ DynamicKeyword/Program.cs
@@ -1,2 +1,69 @@
 ﻿// See https://aka.ms/new-console-template for more information
+#pragma warning disable IDE0059 // Unnecessary assignment of a value
+using Microsoft.CSharp.RuntimeBinder;
+using System.Diagnostics;
+using System.Reflection;
+
 Console.WriteLine("Hello, World!");
+
+ImplicitlyTypedVariable();
+Console.WriteLine();
+ChangeDynamicDataType();
+Console.WriteLine();
+InvokeMembersOnDynamicData();
+Console.WriteLine();
+InvokeMethodWithDynamicKeyword();
+
+Console.ReadLine();
+
+static void ChangeDynamicDataType()
+{
+    Console.WriteLine("-> Change Dynamic Data Type");
+    dynamic t = "Hello";
+    Console.WriteLine($"t is of type {t.GetType()}");
+
+    t = false;
+    Console.WriteLine($"t is of type {t.GetType()}");
+
+    t = new List<int>();
+    Console.WriteLine($"t is of type {t.GetType()}");
+}
+static void ImplicitlyTypedVariable()
+{
+    Console.WriteLine("-> Implicitly Typed Variable");
+    //  'a' is of type List<int>
+    var a = new List<int> { 90 };
+    //  The following causes a compile-time error!
+    //a = "Hello!";
+}
+static void InvokeMembersOnDynamicData()
+{
+    Console.WriteLine("-> Invoke Members On Dynamic Data");
+    dynamic text = "Hello";
+
+    try
+    {
+        Console.WriteLine(text.ToUpper());
+        Console.WriteLine(text.toupper());
+        Console.WriteLine(text.Foo(10, "ee", DateTime.Now));
+    }
+    catch (RuntimeBinderException ex)
+    {
+        Console.WriteLine($"RuntimeBinderException: {ex.Message}");
+    }
+}
+static void InvokeMethodWithDynamicKeyword()
+{
+    Console.WriteLine("-> Invoke Method With Dynamic Keyword");
+    try
+    {
+        Assembly asm = Assembly.LoadFrom("CarLibrary");
+        Type miniVan = asm.GetType("CarLibrary.MiniVan");
+        dynamic mv = Activator.CreateInstance(miniVan);
+        mv.TurboBoost();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
---
+++ DynamicKeyword/VeryDynamicClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicKeyword
+{
+    internal class VeryDynamicClass
+    {
+        private static dynamic _myDynamicField;
+
+        public dynamic DynamicProperty { get; set; }
+
+        public dynamic DynamicMethod(dynamic dynamicParam)
+        {
+            dynamic dynamicLocalVar = "Local Variable";
+
+            int myInt = 10;
+
+            if (dynamicParam is int)
+            {
+                return dynamicLocalVar;
+            }
+
+            return myInt;
+        }
+    }
+}
